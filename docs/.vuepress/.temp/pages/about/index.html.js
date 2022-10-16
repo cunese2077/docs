6690df008999ede14f9424bea2fe5dc062f1f3ba
xiaoyang <2425261752@qq.com>
2022-10-16 11:35:16 +0800

    6-修改部署

--- docs/.vuepress/.temp/pages/about/index.html.js
+++ docs/.vuepress/.temp/pages/about/index.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-74bc627b\",\"path\":\"/about/\",\"title\":\"关于\",\"lang\":\"zh-CN\",\"frontmatter\":{\"lang\":\"zh-CN\",\"title\":\"关于\",\"description\":\"关于页面的描述\"},\"excerpt\":\"\",\"headers\":[],\"git\":{\"updatedTime\":1665038735000},\"filePathRelative\":\"about/README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/about/test.html.js
+++ docs/.vuepress/.temp/pages/about/test.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-524c3f6c\",\"path\":\"/about/test.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[],\"git\":{\"updatedTime\":1665038735000},\"filePathRelative\":\"about/test.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
